b845c029ce35a7f175cbc1af8d44a11ed313fdc8
Zuojun Lin <zuli4207@colorado.edu>
2024-04-18 17:02:28 -0600

    weather

--- ProjectSourceCode/src/resources/js/weatherScript.js
+++ ProjectSourceCode/src/resources/js/weatherScript.js
@@ -1,5 +1,5 @@
 //local weather!!!!!!here
-const container = document.querySelector(".container");
+const container = document.querySelector("#weathercontainer");
 const search = document.querySelector(".search-box button");
 const weatherBox = document.querySelector(".weather-box");
 const weatherDetails = document.querySelector(".weather-details");
@@ -41,27 +41,27 @@ search.addEventListener("click", () => {
 
       switch (json.weather[0].main) {
         case "Clear":
-          image.src = "../images/clear.png";
+          image.src = "../../resources/images/clear.png";
           break;
 
         case "Rain":
-          image.src = "../images/rain.png";
+          image.src =  "../../resources/images/rain.png";
           break;
 
         case "Snow":
-          image.src = "../images/snow.png";
+          image.src =  "../../resources/images/snow.png";
           break;
 
         case "Clouds":
-          image.src = "../images/cloud.png";
+          image.src =  "../../resources/images/cloud.png";
           break;
 
         case "Haze":
-          image.src = "../images/mist.png";
+          image.src =  "../../resources/images/mist.png";
           break;
 
         default:
-          image.src = "";
+          image.src =  "";
       }
 
       temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
