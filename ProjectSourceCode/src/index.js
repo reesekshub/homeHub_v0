6eed57dbf3dec0ce841d2bfbd7672b33f01d1442
pranavkp <pranavkkp4@gmail.com>
2024-04-21 09:16:29 -0600

    finished first part of change password

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -148,7 +148,34 @@ app.post("/user/password", auth, async (req, res) => {
   console.log(oldPassword, newPassword, confirmPassword, "password")
   // Add logic to verify old password, match new and confirm passwords,
   // hash the new password, and update it in the database
-  res.redirect("/profile"); // Redirect to profile page after password change
+  const oldHashPassword = await bcrypt.hash(oldPassword, 10);
+  const newHashPassword = await bcrypt.hash(newPassword, 10);
+  const query = "SELECT * FROM users WHERE username = $1";
+  const username = req.session.user.username
+  const userDetails = await db.oneOrNone(query, username)
+  console.log("PASSWORD", req.body)
+  if (userDetails.password === oldHashPassword && userDetails.username === username) {
+    try {
+      // Insert username and hashed password into the 'users' table
+      await db.none("UPDATE users SET PASSWORD =$2 WHERE USERNAME = $1", [
+        username,
+        newHashPassword,
+      ]);
+      res.status(400).send({ message: "You have successfully changed password" })
+    } catch (err) {
+      console.log(err)
+      console.log("pass", oldHashPassword, newHashPassword)
+      res.status(500).send(err)
+      // res.redirect("/profile"); // Redirect to profile page after password change
+    }
+  } else {
+    console.log("ELSE", userDetails, oldHashPassword, newHashPassword)
+    res.redirect("/profile")
+  }
+
+
+
+
 });
 
 // Login
