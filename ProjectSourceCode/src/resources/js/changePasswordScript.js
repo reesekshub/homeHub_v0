ceb3b933901531523cc07cc9e98e9191dd4e0805
pranavkp <pranavkkp4@gmail.com>
2024-04-21 18:28:10 -0600

    Finished confirm password functionality

--- ProjectSourceCode/src/resources/js/changePasswordScript.js
+++ ProjectSourceCode/src/resources/js/changePasswordScript.js
@@ -1,3 +1,25 @@
 const formEl = document.getElementById("passForm")
 const buttonEl = document.getElementById("passButton")
+const newPasswordEl = document.getElementById("newPassword")
+const confirmPasswordEl = document.getElementById("confirmPassword")
+const toolTipEl = document.getElementById("tooltip")
 buttonEl.setAttribute("disabled", true)
+confirmPasswordEl.addEventListener("keyup", checkPassword)
+
+
+function checkPassword() {
+    var newPassword = newPasswordEl.value
+    var confirmPassword = confirmPasswordEl.value
+    console.log(newPassword, confirmPassword)
+    if (newPassword === confirmPassword) {
+        buttonEl.removeAttribute("disabled")
+        toolTipEl.innerText = ""
+    } else {
+        toolTipEl.innerText = "Passwords don't match"
+    }
+
+
+
+
+
+}
